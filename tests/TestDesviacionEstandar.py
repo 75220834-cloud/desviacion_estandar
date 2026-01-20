1cb0bba8473164dc7a02b03e29488704394acc3c
ItaloRey <75220834@continental.edu.pe>
2026-01-19 19:20:55 -0500

    Caso 3: Desviación estándar con varios elementos (Rojo)

--- tests/TestDesviacionEstandar.py
+++ tests/TestDesviacionEstandar.py
@@ -13,3 +13,8 @@ class TestDesviacionEstandar(unittest.TestCase):
         datos = [5]
         desviacion = DesviacionEstandar(datos)
         self.assertEqual(0, desviacion.calcular())
+
+    def test_lista_varios_elementos_retorna_desviacion(self):
+        datos = [2, 4, 4, 4, 5, 5, 7, 9]
+        desviacion = DesviacionEstandar(datos)
+        self.assertEqual(2, desviacion.calcular())
