f12d54fda21ee4e676febbd431b718c4c3745ef8
ItaloRey <75220834@continental.edu.pe>
2026-01-19 19:22:12 -0500

    Caso 3: Desviación estándar con varios elementos (Verde)

--- src/logica/DesviacionEstandar.py
+++ src/logica/DesviacionEstandar.py
@@ -3,7 +3,21 @@ class DesviacionEstandar:
         self.__datos = datos
 
     def calcular(self):
-        if len(self.__datos) == 0:
+        n = len(self.__datos)
+
+        if n == 0:
             return None
-        if len(self.__datos) == 1:
+
+        if n == 1:
             return 0
+
+        # media
+        media = sum(self.__datos) / n
+
+        # suma de cuadrados
+        suma = 0
+        for x in self.__datos:
+            suma += (x - media) ** 2
+
+        # desviación estándar poblacional
+        return (suma / n) ** 0.5
